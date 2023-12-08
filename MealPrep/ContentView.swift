846dfdb09ec1538d4dc1a197a8e53dd1526fac87
sstuden <sstuden@cs.washington.edu>
2023-12-08 13:31:48 -0800

    Initial Commit

---
+++ MealPrep/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MealPrep
+//
+//  Created by Simon Studen on 12/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MealPrep/MealPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPrepApp.swift
+//  MealPrep
+//
+//  Created by Simon Studen on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MealPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
